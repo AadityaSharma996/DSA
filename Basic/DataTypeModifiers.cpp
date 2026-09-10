fe1577d3dd936ab893161f66d82c16d824f870cf
AadityaSharma996 <aadityasharmaas96@gmail.com>
2026-09-11 00:05:21 +0530

    DSA

---
+++ Basic/DataTypeModifiers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    //DTM
+    cout << sizeof (int) << endl;
+    cout << sizeof (long int) << endl; //Long
+    cout << sizeof (short int) << endl; //Short
+    cout << sizeof (long long) << endl; //Long Long
+
+    unsigned int x = -10; //Unsigned
+    cout << x << endl;
+}
