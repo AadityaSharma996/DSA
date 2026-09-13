e3650b6f6548fa4ecc205e3f48a8adbc71c00d22
AadityaSharma996 <aadityasharmaas96@gmail.com>
2026-09-13 16:14:12 +0530

    DSA

--- Basic/KadaneAlgo.cpp
+++ Basic/KadaneAlgo.cpp
@@ -17,13 +17,13 @@ int main () {
 
     int n = 5;
     int arr[5] = {1,2,3,4,5};
-    int maxSum = INT16_MIN;
+    int maxSum = INT16_MIN; //To compare the array value with the minimum -infinity value
 
     for (int st=0; st<n; st++){
         int currentSum = 0;
         for (int end=st; end<n; end++){
-            currentSum += arr[end];
-            maxSum = max(currentSum, maxSum);
+            currentSum += arr[end]; //Here we dont add the previous value again the sum will be added by its alr present sum from earlier
+            maxSum = max(currentSum, maxSum); //This will give us the current maximum sum value of the array
         }
     }
     cout << "MaxSum is: " << maxSum << endl;
