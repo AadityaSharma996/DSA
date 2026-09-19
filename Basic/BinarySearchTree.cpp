38f848e1e267d049a20b5cb5f9e392c9e811b98a
AadityaSharma996 <aadityasharmaas96@gmail.com>
2026-09-19 16:44:36 +0530

    DSA

--- Basic/BinarySearchTree.cpp
+++ Basic/BinarySearchTree.cpp
@@ -2,6 +2,30 @@
 #include <vector>
 using namespace std;
 
+//Recursive BinarySearch
+// int recBinarySearch(vector<int> arr, int tar, int st, int end)
+// {
+//     if (st <= end)
+//     {
+//         int mid = st + (end - st) / 2;
+//         if (tar > arr[mid])
+//         { // 2nd Half
+//             return recBinarySearch(arr, tar, mid + 1, end);
+//         }
+//         else if (tar < arr[mid])
+//         { // 1st Half
+//             return recBinarySearch(arr, tar, st, mid - 1);
+//         }
+//         else
+//         { // mid => ans
+//             return mid;
+//         }
+//     }
+//     return -1;
+// }
+
+// For BinarySearch
+
 int binarySearch(vector<int> arr, int tar){
     int st=0, end = arr.size()-1;
 
@@ -10,7 +34,7 @@ int binarySearch(vector<int> arr, int tar){
         // wrong mid value do to solve this problem we use
 
         int mid = st + (end-st) / 2; //This is the most optimised solution for this problem
-        
+
         if (tar > arr[mid]){
             st = mid + 1;
         } else if (tar < arr [mid]){
@@ -23,15 +47,16 @@ int binarySearch(vector<int> arr, int tar){
 
 }
 
-int main(){
+int main()
+{
 
-    vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12}; //Odd
-    int tar1= 12;
+    vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12}; // Odd
+    int tar1 = 12;
 
-    //cout << binarySearch(arr1, tar1) << endl;
+    // cout << binarySearch(arr1, tar1) << endl;
 
-    vector<int> arr2 = {-1, 0, 3, 4, 5, 9, 12}; //Even
-    int tar2= 0;
+    vector<int> arr2 = {-1, 0, 3, 4, 5, 9, 12}; // Even
+    int tar2 = 0;
 
     cout << binarySearch(arr2, tar2) << endl;
 }
